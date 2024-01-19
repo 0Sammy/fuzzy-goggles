6c6833a9ebc879e16352859addd1cdc02ea865c4
Priest <Charleschukwuemeka47@gmail.com>
2024-01-19 07:28:33 +0100

    Fix: Change the tracking code generation

--- src/lib/GenerateTrackingNumber.ts
+++ src/lib/GenerateTrackingNumber.ts
@@ -1,5 +1,5 @@
 const generateRandomNumber = (): string => {
-    const prefix = "UT-";
+    const prefix = "TEH-";
     const randomNumber = Math.floor(100000 + Math.random() * 900000).toString();
     const formattedNumber = `${prefix}${randomNumber}`;
     return formattedNumber;
